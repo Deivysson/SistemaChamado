4e997014237a8a18ed775e515ea3e602763a75db
Deivysson Gomes <105027772+Deivysson@users.noreply.github.com>
2023-10-02 20:51:46 -0300

    primeiro commit

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -6,62 +6,138 @@ import avatar from '../../assets/avatar.png'
 import { AuthContext } from '../../contexts/auth'
 import './profile.css'
 
+import { db, storage } from '../../services/firebaseConnection'
+import { doc, updateDoc } from 'firebase/firestore'
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+
+import { toast } from 'react-toastify'
+
 export default function Profile(){
 
-    const { user, storareUser, setUser, logout } = useContext(AuthContext);
+    const { user, storageUser, setUser, logout } = useContext(AuthContext);
+
+    const [imageAvatar, setImageAvatar] = useState(null);
     const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl)
     const [nome, setNome] = useState(user && user.nome)
     const [email, setEmail] = useState(user && user.email)
 
 
     function handleFile(e) {
-        
+        if(e.target.files[0]){
+            const image = e.target.files[0];
+
+            if(image.type === 'image/jpeg' || image.type === 'image/png') {
+                setImageAvatar(image)
+                setAvatarUrl(URL.createObjectURL(image))
+            }else{
+                alert("Envie uma imagem do tipo PNG ou JPEG")
+                setImageAvatar(null);
+                return;
+            }
+        }
+    }
+
+
+    async function handleUpload(){
+       const currentUid = user.uid;
+       
+    const uploadRef = ref(storage, `images/${currentUid}/${imageAvatar.name}` )
+
+    const uploadTask = uploadBytes(uploadRef, imageAvatar)
+    .then((snapshot) => {
+        getDownloadURL(snapshot.ref).then(async (downloadURL) => {
+            let urlFoto = downloadURL;
+
+            const docRef = doc(db, "users", user.uid)
+            await updateDoc(docRef, {
+                avatarUrl: urlFoto,
+                nome: nome,
+            })
+            .then(() => {
+                let data = {
+                    ...user,
+                    nome: nome,
+                    avatarUrl: urlFoto,
+                }
+                setUser(data);
+                storageUser(data);
+                toast.success("Atualizado!")
+            })
+        })
+    })
+    }
+
+
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        if(imageAvatar === null && nome !== ''){
+            // Atualizar apenas o nome do user
+            const docRef = doc(db, "users", user.uid)
+            await updateDoc(docRef, {
+                nome: nome,
+            })
+            .then(() => {
+                let data = {
+                    ...user,
+                    nome: nome,
+                }
+                setUser(data);
+                storageUser(data);
+                toast.success("Atualizado!")
+            })
+        }else if(nome !== '' && imageAvatar !== null){
+            //Atualizar nome e foto
+            handleUpload()
+        }
+
     }
 
 
     return(
-    <div>
-        <Header />
+<div>
+    <Header />
 
-        <div className='content'>
-            <Title name="Minha conta">
-                 <FiSettings size={25} />
-            </Title>
+    <div className='content'>
+         <Title name="Minha conta">
+             <FiSettings size={25} />
+         </Title>
 
-        <div className='container'>
-            <form className='form-profile'>
-                 <label className='label-avatar'>
-                     <span>
-                         <FiUpload color='#fff' size={25} />
-                     </span>
+    <div className='container'>
+         <form className='form-profile' onSubmit={handleSubmit}>
+            <label className='label-avatar'>
+                <span>
+                    <FiUpload color='#fff' size={25} />
+                </span>
 
-                     <input type='file' accept='image/*' onChange={handleFile} /> <br/>
-                     {avatarUrl === null ? (
-                         <img src={avatar} alt='Foto de perfil' width={250} height={250} />
+                <input type='file' accept='image/*' onChange={handleFile} /> <br/>
+                    {avatarUrl === null ? (
+                 <img src={avatar} alt='Foto de perfil' width={250} height={250} />
 
-                    ) : (
-                          <img src={avatarUrl} alt='Foto de perfil' width={250} height={250} />
-                    )}
+                 ) : (
+                 <img src={avatarUrl} alt='Foto de perfil' width={250} height={250} />
+                 )}
 
-                </label>
+            </label>
 
-                <label>Nome</label>
-                <input type='text' value={nome} onChange={(e) => setNome(e.target.value)} />
+              <label>Nome</label>
+              <input type='text' value={nome} onChange={(e) => setNome(e.target.value)} />
 
-                <label>Email</label>
-                <input type='text' value={email} disabled={true} />
+              <label>Email</label>
+            <input type='text' value={email} disabled={true} />
 
-                <button type='submit'>Salvar</button>
+              <button type='submit'>Salvar</button>
 
             </form>
 
-        </div>
+    </div>
 
-        <div className='container'>
-            <button className='logout-btn' onClick={() => logout() }>Sair</button>
-        </div>
+    <div className='container'>
+         <button className='logout-btn' onClick={() => logout() }>Sair</button>
+    </div>
 
-        </div>
+    </div>
 
     </div>
     )
